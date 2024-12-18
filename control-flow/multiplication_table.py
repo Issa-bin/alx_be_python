71f0d24ef62695324ca9f4c17fa88c8c04c88863
issa <emmanuelokatch9@gmail.com>
2024-12-18 16:13:09 +0300

    multiplication tab generator

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -3,7 +3,7 @@ number = float(input("Enter a number to see its multiplication table:"))
 
 #generate and print multiplication table
 print(f"multiplication table for {number}:")
-for float in range (1,10):
+for float in range (1,11):
     product = number * float
 
 #print result
