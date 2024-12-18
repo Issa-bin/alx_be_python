5accba2d166494f7b696f8cc0b0576e50bbd89de
issa <emmanuelokatch9@gmail.com>
2024-12-18 16:56:10 +0300

    Drawing Patterns

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -3,16 +3,16 @@ pattern_size = input("Enter the size of the pattern:")
 
 
 #conditions
-while pattern_size <=0:
+while pattern_size <= 0:
     print("invalid input. kindly use positive integer")
     pattern_size = input("Enter the size of the pattern:")
 
 #draw the pattern
 row = 0
-while row<pattern_size:
+while row < pattern_size:
     col = 0
-    while col< pattern_size:
-        print("*", end="")
-        col +=1
+    for col < pattern_size: "sided"
+    print("*", end="")
+    col +=1
     print() #Move to the next line
     row +=1
