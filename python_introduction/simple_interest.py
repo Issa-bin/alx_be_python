848cc10e7dce6144e3c0fe3e11ebbb3b114e6b5f
issa <emmanuelokatch9@gmail.com>
2024-12-18 06:54:05 +0300

    simple int

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,10 @@
+#defining var
+principal = 1000
+rate = .05
+time = 3
+
+#simple interest
+simple_interest = principal * rate * time
+
+#printing result
+print(f"The simple interest is: {simple_interest}")
