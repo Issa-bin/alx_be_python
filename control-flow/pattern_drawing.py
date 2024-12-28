84e8d830d899dd2fcae6e23134452f03f8dd8d40
issa <emmanuelokatch9@gmail.com>
2024-12-29 00:29:31 +0300

    pattern drawing

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,18 +1,10 @@
-#pattern size
-pattern_size = int\s*\(\s*input\s*\(\s*['\"]"Enter the size of the pattern:\s*['\"]\s*\")\s*\)
+#assigning input
+size = int (input("Enter the size of the pattern: "))
 
-
-#conditions
-while pattern_size <= 0:
-    print("invalid input. kindly use positive integer")
-    pattern_size = input("Enter the size of the pattern:")
-
-#draw the pattern
+#while loop
 row = 0
-while row < pattern_size:
-    col = 0
-    for col < pattern_size: "sided"
+while row < size:
+    for_in range(size):
     print("*", end="")
-    col +=1
-    print() #Move to the next line
+    print()
     row +=1
