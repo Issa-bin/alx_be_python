47e3d25e01ec9d56c29f1bab9d7978adb4d442dd
issa <emmanuelokatch9@gmail.com>
2025-01-11 22:58:13 +0300

    Arithmetic operations

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -4,10 +4,11 @@ def main():
     print("Arithmetic Operations")
     num1 = float(input("Enter the first number: "))
     num2 = float(input("Enter the second number: "))
-    operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
+    operation = input("Choose the operation (+, -, *, /):")
 
-    result = perform_operation(*num1*num2* operation)
+#Perform operations
+    result = perform_operation (num1, num2, operation)
     print(f"Result: {result}")
-
+    
 if __name__== "__main__":
     main()
