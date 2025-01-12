cee48ff5b382253b192561ac7bef51531bece52e
issa <emmanuelokatch9@gmail.com>
2025-01-12 17:49:49 +0300

    Explore datetime

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,59 +1,17 @@
-def explore_datetime():
-    #import the datetime module
-    import datetime
+from datetime import datetime, timedelta
+    
+def display_current_datetime():
+    current_date = datetime.now().strftime("%Y-%m-%d %H:%M-%S")
+    print("Current date and time: ",current_date)
 
-    #Get today's date and time 
-    today = datetime.datetime.now()
 
-    #Print today's date nd time
-    print(f"Today's date and time: {today}")
+display_current_datetime()
 
-    #Access individual components of the datetime object
-    year = today.year
-    month = today.month
-    day = today.day
-    hour = today.hour
-    minute = today.minute
-    second = today.second
+number_of_days = int(input("Enter the number of days to add to the current: "))
 
-    #Print individual components
-    print(f"Year: {year}")
-    print(f"Month: {month}")
-    print(f"Day: {day}")
-    print(f"Hour: {hour}")
-    print(f"Minute: {minute}")
-    print(f"Second: {second}")
+def calculate_future_date():
+    future_date = datetime.now() + timedelta(days=number_of_days)
+    calculated_future_date = future_date.strftime("%Y-%m-%d")
+    print("Future date: ",calculated_future_date)
 
-    #Create a specific date and time object
-    specific_datetime = datetime.datetime(2025,01,07,00,25,0)#Year,Month,Day,Hour,Minute,Second
-
-    #print the specific date and time object
-    print(f"\nSpecific date and time: {specific_datetime}")
-
-    #Perform time arithmetic - Add or subtract time from date and time object
-    time_delta = datetime.timedelta(days=2, hours=5) # Create a timedelta object (days, hours, minutes, seconds)
-    future_datetime = today + time_delta
-
-    #Print the future date and time
-    print(f"nDate and time after adding time delta: {future_datetime}")
-
-    #Formatting datetime objects
-    formatted_date = today.strftime("%Y-%m-%d") #YYYY-MM-DD format
-    formatted_time = today.strftime("%H:%M:%S") #HH:MM:SS format
-
-    #Print formatted date and time
-    print(f"\nFormatted date: {formatted_date}")
-    print(f"Formatted time: {formatted_time}")
-
-    #get the current date and time using datetime.now()
-    now = datetime.now()
-    print('Current date and time:',now)
-
-    #add 3 days to the current date using timedelta
-    number_of_days = 3
-    future_date = now + datetime.timedelta(days = number_of_days)
-    print('Date and time after adding 3 days:',future_date)
-
-#access individual components
-if__name__=='__main__':
-explore_datetime() 
+calculate_future_date()
