be37c7571d1294eb469bc1995825d6f51fbf382d
issa <emmanuelokatch9@gmail.com>
2025-01-19 13:43:01 +0300

    bank account

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -8,12 +8,13 @@ class BankAccount:
             self.account_balance += amount
             return f"Deposited: ${amount:2f}"
     def withdraw(self, amount):
-            if amount > self.account_balance:
-                return "Insufficient funds."
+            if amount > self.account_balance or amount <= 0:
+                return False #withdrawal failed
             elif amount <= 0:
                     return "Insufficient funds."
             else:
                 self.account_balance -= amount
-                return f"Withdraw: ${amount:.2f}"
+                return True #withdrawal successful
+            
     def display_balance(self):
             print(f"Current Balance: ${self.account_balance:.2f}")
